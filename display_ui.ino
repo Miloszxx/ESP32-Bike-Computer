aca7272a33f59be0d6a600ec377acc96a56128d3
xpolaczeqx@gmail.com <xpolaczeqx@gmail.com>
2026-08-05 12:55:26 +0200

    fix(ui): fix route progress text flickering, colors and position

--- display_ui.ino
+++ display_ui.ino
@@ -78,11 +78,10 @@ void refreshMainScreenWithMap() {
   if (gpxLoaded && totalRouteDistance > 0.1) {
     int routeProgress = (int)((tripA / totalRouteDistance) * 100.0);
     if (routeProgress > 100) routeProgress = 100; 
-    
-    tft.setTextColor(TFT_BLACK, TFT_WHITE);   
-    tft.setTextDatum(TL_DATUM);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);   
+    tft.setTextDatum(BL_DATUM);
     tft.setTextPadding(40);
-    tft.drawString(String(routeProgress) + "%", 5, 240, 2); 
+    tft.drawString(String(routeProgress) + "%", 5, 270, 2); 
     tft.setTextPadding(0);
   }
 }
